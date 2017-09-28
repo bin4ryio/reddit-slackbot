2d4d27207d79e57185fa4b0cfa2bcb9b82408cc9
XavierAdam <gheyfheg1997@gmail.com>
2017-09-28 09:37:29 +0800

    Testing scheduler

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import datetime
 import os
-import praw
+import sys
+
 from random import randint
+import praw
 from slackclient import SlackClient
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -11,13 +13,14 @@ REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
 REDDIT_CLIENT_USER_AGENT = os.getenv('REDDIT_CLIENT_USER_AGENT')
 
 subreddits = [
-  # { 'c': 'reddit', 's': 'funny' },
-  # { 'c': 'reddit', 's': 'dailyprogrammer' },
-  # { 'c': 'reddit', 's': 'programmerhumor' },
-  # { 'c': 'reddit', 's': 'hackernews' },
-  # { 'c': 'reddit', 's': 'technology' },
-  # { 'c': 'reddit', 's': 'learnprogramming' },
-  # { 'c': 'reddit', 's': 'todayilearned' },
+  {'c': 'reddit', 's': 'funny'},
+  {'c': 'reddit', 's': 'dailyprogrammer'},
+  {'c': 'reddit', 's': 'programmerhumor'},
+  {'c': 'reddit', 's': 'hackernews'},
+  {'c': 'reddit', 's': 'technology'},
+  {'c': 'reddit', 's': 'ProgrammerTIL'},
+  {'c': 'reddit', 's': 'todayilearned'},
+  '''
   { 'c': 'design', 's': 'art' },
   { 'c': 'design', 's': 'web_design' },
   { 'c': 'funny', 's': 'funny' },
@@ -25,9 +28,11 @@ subreddits = [
   { 'c': 'learnprogramming', 's': 'dailyprogrammer' },
   { 'c': 'hackernews', 's': 'hackernews' },
   { 'c': 'hackernews', 's': 'technology' },
-  { 'c': 'learnprogramming', 's': 'learnprogramming' },
+  { 'c': 'learnprogramming', 's': 'ProgrammerTIL' },
   { 'c': 'todayilearned', 's': 'todayilearned' },
+  '''
 ]
+
 
 def get_posts(payload):
   r = praw.Reddit(
@@ -43,12 +48,13 @@ def get_posts(payload):
       title_list.append(str(submission.title))
       url_list.append(str(submission.url))
     # return random.choice(url_list)
-    i = randint(0, len(url_list)-1)
+    i = randint(0, len(url_list) - 1)
     return '{} {}'.format(title_list[i], url_list[i])
 
   except (praw.errors.PRAWException, praw.errors.HTTPException) as e:
     print(e)
     pass
+
 
 def getLastRead():
   try:
@@ -62,33 +68,33 @@ def getLastRead():
     lastRead = 0
   return lastRead
 
+
 def getUpdatedLastRead(val):
   return val + 1 if (val + 1) <= len(subreddits) - 1 else 0
 
+
 def main():
-  '''
-  sc = SlackClient(BOT_TOKEN)
-  if sc.rtm_connect():
-    lastRead = getLastRead()
-    updatedLastRead = getUpdatedLastRead(lastRead)
+  hour = datetime.datetime.today().hour
+  if (hour >= 2 & hour <= 14) & hour % 2 == 0:
+    sc = SlackClient(BOT_TOKEN)
+    if sc.rtm_connect():
+      lastRead = getLastRead()
+      updatedLastRead = getUpdatedLastRead(lastRead)
 
-    sc.api_call(
-      "chat.postMessage",
-      username="Digibear",
-      channel=subreddits[updatedLastRead]['c'],
-      text=get_posts(subreddits[updatedLastRead]['s']),
-      unfurl_links="true"
-    )
-    # sc.rtm_send_message(subs['c'], get_posts(subs['s']))
+      sc.api_call(
+        "chat.postMessage",
+        username="Digibear",
+        channel=subreddits[updatedLastRead]['c'],
+        text=get_posts(subreddits[updatedLastRead]['s']),
+        unfurl_links="true"
+      )
+      # sc.rtm_send_message(subs['c'], get_posts(subs['s']))
 
-    wr = open('lastRead', 'w')
-    wr.write(str(updatedLastRead))
-    wr.close()
-
-    '''
-
-    print(datetime.datetime.today())
-
+      wr = open('lastRead', 'w')
+      wr.write(str(updatedLastRead))
+      wr.close()
+  else:
+    print('The time is not right!')
 
 if __name__ == "__main__":
   main()
